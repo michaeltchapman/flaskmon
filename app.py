2cca26bced18823cba6c26d1165b9dfebfc5c630
Michael <mxc900@os-mon.(none)>
2012-07-19 15:59:30 +1000

    pulls in data from ganglia and throws it into the popups as a first attempt. Need to add graphing stuff now

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
+from rrdtool import fetch
+import time
+from os import listdir
 
 app = Flask(__name__)
 db = create_engine('postgresql://nova:testing@os-sql.os/nova')
@@ -10,8 +13,8 @@ session = Session()
 
 metadata = MetaData(db)
 
-services = Table('services', metadata, autoload=True)
-instances = Table('instances', metadata, autoload=True)
+sqlservices = Table('services', metadata, autoload=True)
+sqlinstances = Table('instances', metadata, autoload=True)
 
 @app.route('/')
 def hello_world():
@@ -21,23 +24,32 @@ def hello_world():
 
 @app.route('/fluid')
 def fluid():
-    result = dict()
+
+    # list of nodes avabilable from ganglia
+    nodes = dict()
+    nodelist = listdir('/var/lib/ganglia/rrds/unspecified')
+
+    for node in nodelist:
+        if node != '__SummaryInfo__':
+            nodes[node.split('.')[0]] = dict()
+            nodes[node.split('.')[0]]['domain'] = node.split('.')[1]
 
     #### SERVICES ####
 
     # retrieve service statuses from nova database
-    for row in session.query(services):
-        if row.topic.encode("utf-8") not in result:
-            result[row.topic.encode("utf-8")] = list()
-        result[row.topic.encode("utf-8")].append((row.host.encode("utf-8"),{"disabled" : row.disabled }))
+    for row in session.query(sqlservices):
+        if row.host.encode("utf-8") not in nodes:
+            print row.host.encode("utf-8")
+            pass
+        nodes[row.host.encode("utf-8")][row.topic.encode("utf-8") + '-disabled'] = row.disabled
 
     # query sql server status
-    r = session.query("Tuples Returned", "Tuples Fetched", "Transactions Committed", "Blocks Fetched", "Block Cache Hits").from_statement('select pg_stat_get_db_tuples_returned(1) as "Tuples Returned", pg_stat_get_db_tuples_fetched(1) as "Tuples Fetched", pg_stat_get_db_xact_commit(1) as "Transactions Committed", pg_stat_get_db_blocks_fetched(1) as "Blocks Fetched", pg_stat_get_db_blocks_hit(1) as "Block Cache Hits"').all()[0]
-    d = dict()
-    for row in r.keys():
-        d[row] = r.__dict__[row]
+#    r = session.query("Tuples Returned", "Tuples Fetched", "Transactions Committed", "Blocks Fetched", "Block Cache Hits").from_statement('select pg_stat_get_db_tuples_returned(1) as "Tuples Returned", pg_stat_get_db_tuples_fetched(1) as "Tuples Fetched", pg_stat_get_db_xact_commit(1) as "Transactions Committed", pg_stat_get_db_blocks_fetched(1) as "Blocks Fetched", pg_stat_get_db_blocks_hit(1) as "Block Cache Hits"').all()[0]
+#    d = dict()
+#    for row in r.keys():
+#        d[row] = r.__dict__[row]
     
-    result['sql'] = [("os-sql", d)]
+    #nodes['os-sql'] = d
 
     # query amqp server status
 
@@ -52,18 +64,17 @@ def fluid():
     # use rrdtool to get load of each server
         # only display those that are > 1
     # requests/sec
-   
-    #### INSTANCES ####
 
-    # grab current running instances from nova db
+    res = 600 # 5 minutes
+    t = int(time.mktime(time.localtime(time.time())))
 
-    #### OBJECT STORE ####
+    for node in nodes:
+        metrics = listdir('/var/lib/ganglia/rrds/unspecified/' + node + '.' + nodes[node]['domain'])
+        for metric in metrics:
+            nodes[node][metric.split('.')[0]] = fetch('/var/lib/ganglia/rrds/unspecified/'  + node + '.' + nodes[node]['domain'] + '/' + metric, 'AVERAGE', '-r ' + str(res), '-s e-30m', '-e ' + str(t/res*res))[2]
 
-    #### IMAGE STORE ####
-
-    #### IDENTITY SERVICE ####
-
-    return render_template('fluid.html', result=result)
+            
+    return render_template('fluid.html', nodes=nodes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+from os import listdir 
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
-
+from rrdtool import fetch
+import time
 
 db = create_engine('postgresql://nova:testing@os-sql.os/nova')
 
@@ -19,7 +21,6 @@ for row in session.query(services):
         result[row.topic.encode("utf-8")] = list()
     result[row.topic.encode("utf-8")].append((row.host.encode("utf-8"),{"status" : row.disabled }))
 
-
 r = session.query("tupret", "tupfet", "transactions", "blocksfetched", "blockhit").from_statement('select pg_stat_get_db_tuples_returned(1) as tupret, pg_stat_get_db_tuples_fetched(1) as tupfet, pg_stat_get_db_xact_commit(1) as transactions, pg_stat_get_db_blocks_fetched(1) as blocksfetched, pg_stat_get_db_blocks_hit(1) as blockhit').all()[0]
 d = dict()
 for row in r.keys():
@@ -27,4 +28,35 @@ for row in r.keys():
     
 result['sql'] = ("os-sql", d)
 
-print result
+res=60
+t = int(time.mktime(time.localtime(time.time())))
+a = t/res*res
+#print a
+f = fetch('/var/lib/ganglia/rrds/unspecified/os-amqp.os/load_one.rrd', 'AVERAGE', '-r ' + str(res), '-s e-5m', '-e ' + str(a) )
+
+print f
+
+for line in f[2]:
+    print line[0]
+
+
+
+res=600
+t = int(time.mktime(time.localtime(time.time())))
+a = t/res*res
+
+nodes = dict()
+nodelist = listdir('/var/lib/ganglia/rrds/unspecified')
+
+for node in nodelist:
+    if node != '__SummaryInfo__':
+        nodes[node.split('.')[0]] = dict()
+        nodes[node.split('.')[0]]['domain'] = node.split('.')[1]
+
+for node in nodes:
+    print nodes[node]
+    metrics = listdir('/var/lib/ganglia/rrds/unspecified/' + node + '.' + nodes[node]['domain'])
+    for metric in metrics:
+        nodes[node][metric.split('.')[0]] = fetch('/var/lib/ganglia/rrds/unspecified/'  + node + '.' + nodes[node]['domain'] + '/' + metric, 'AVERAGE', '-r ' + str(res), '-s e-30m', '-e ' + str(t/res*res))[2]
+
+
