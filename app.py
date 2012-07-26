6e99e2b98de7721ef3eb8d007d8fdfc02100d601
Michael <mxc900@os-mon.(none)>
2012-07-26 14:36:12 +1000

    removed all sql dependencies. Service status is no longer pulled from postgres

--- app.py
+++ app.py
@@ -31,18 +31,18 @@ def fluid():
 
     # retrieve service statuses from nova database
     # should do this from a metric
-    for row in session.query(sqlservices):
-        if row.host.encode("utf-8") not in nodes:
-            print row.host.encode("utf-8")
-            pass
+    #for row in session.query(sqlservices):
+    #    if row.host.encode("utf-8") not in nodes:
+    #        print row.host.encode("utf-8")
+    #        pass
         #nodes[row.host.encode("utf-8")][row.topic.encode("utf-8") + '-disabled'] = row.disabled
 
     # query sql server status
     # do this from a local metric instead of here.
-#    r = session.query("Tuples Returned", "Tuples Fetched", "Transactions Committed", "Blocks Fetched", "Block Cache Hits").from_statement('select pg_stat_get_db_tuples_returned(1) as "Tuples Returned", pg_stat_get_db_tuples_fetched(1) as "Tuples Fetched", pg_stat_get_db_xact_commit(1) as "Transactions Committed", pg_stat_get_db_blocks_fetched(1) as "Blocks Fetched", pg_stat_get_db_blocks_hit(1) as "Block Cache Hits"').all()[0]
-#    d = dict()
-#    for row in r.keys():
-#        d[row] = r.__dict__[row]
+    #    r = session.query("Tuples Returned", "Tuples Fetched", "Transactions Committed", "Blocks Fetched", "Block Cache Hits").from_statement('select pg_stat_get_db_tuples_returned(1) as "Tuples Returned", pg_stat_get_db_tuples_fetched(1) as "Tuples Fetched", pg_stat_get_db_xact_commit(1) as "Transactions Committed", pg_stat_get_db_blocks_fetched(1) as "Blocks Fetched", pg_stat_get_db_blocks_hit(1) as "Block Cache Hits"').all()[0]
+    #d = dict()
+    #for row in r.keys():
+    #    d[row] = r.__dict__[row]
     
     #nodes['os-sql'] = d
 
@@ -52,12 +52,15 @@ def fluid():
     res = 600 # 5 minutes
     t = int(time.mktime(time.localtime(time.time())))
 
+    # need to move things out of 'unspecified" at some point...
     for node in nodes:
         metrics = listdir('/var/lib/ganglia/rrds/unspecified/' + node + '.' + nodes[node]['domain'])
         for metric in metrics:
-            nodes[node][metric.split('.')[0]] = fetch('/var/lib/ganglia/rrds/unspecified/'  + node + '.' + nodes[node]['domain'] + '/' + metric, 'AVERAGE', '-r ' + str(res), '-s e-30m', '-e ' + str(t/res*res))[2]
+            nodes[node][metric.split('.')[0]] = fetch('/var/lib/ganglia/rrds/unspecified/'  
+                            + node + '.' + nodes[node]['domain'] + '/' 
+                            + metric, 'AVERAGE', '-r ' + str(res), 
+                            '-s e-30m', '-e ' + str(t/res*res))[2]
 
-            
     return render_template('fluid.html', nodes=nodes)
 
 if __name__ == '__main__':
